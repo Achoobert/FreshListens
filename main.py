4e6f386d53196c907a4f9901ad97e2de57c914f5
IsaacS <greenace14@gmail.com>
2019-11-06 13:12:26 +0700

    +add db reads and useradd, testing UI
    READ db, 
    get track size, get track location, get user history with tracks*not tested
    display all users, display all library
    Select tracks in order of desired play
    
    testing UI, 
    allow picking of user and track by ID
    pass desired tosend tracks in wanted order of play
    
    ADD user to DB

--- main.py
+++ main.py
@@ -34,29 +34,45 @@ def libraryDatabaseInit():  # check whole library location for new files (IF NOT
             ])
 
     for f in files:
-        print(f)
+        # print(f)
         c.execute(
             # track_id, name, location, type, size
             "INSERT OR IGNORE INTO library VALUES (null,'{0}','{1}','{2}',{3})".format(*f))
     conn.commit()
 
 
-checkDatabase()
+def displayFileHistory(userID):  # return array 'trackID, trackName, givenDate'
+    for row in c.execute("SELECT track_id FROM history where user_id=1"):
+        for row2 in c.execute("SELECT * FROM library where track_id=?", (row)):
+            print(row2[0], row[1])
     # TODO access database using userID
 
-# check library location for new files
-libraryDatabaseInit()
 
-# send files
+def getFileSizeTotal(sendTracks):
+    total = 0
+    for i in sendTracks:
+        c.execute(
+            "SELECT size FROM library WHERE track_id=?", (i,))  # note: needs to be sequence
+        total += (c.fetchone()[0])
+    # TODO is this math actually right?
+    total = (total*1e-6)
+    return int(total)
 
 
-def sendFiles(toSend):
-    # toSend, list of requested files
+def sendFiles(sendTracks, user_id, drive):  # toSend, list of requested files
+    for i in sendTracks:
+        c.execute(
+            "SELECT location FROM library WHERE track_id=?", (i,))  # note: needs to be sequence
+        print("found: ", c.fetchone())
+        # send file
+        send(location, order, drive)
+        # Save sent file to history table
 
-    # Save sent file to history table
+        #
 
 
 def logFileReceived(user, file):
+
     today = date.today()
     # add date sent to user
     # history_id, date , user_id , track_id ,
@@ -79,6 +95,15 @@ def send(location, order, drive):
     print("Success")
 
 
+def addUser():
+    name = input("Enter Listener's Name")
+    location = input("Enter location")
+    # user name, village LOCATION name
+    c.execute("INSERT OR IGNORE INTO users VALUES (?, ?s, ?s)",
+              "", name, location)
+    conn.commit()
+
+
 def reviewSentTracks():
     # just print the whole sql
     print('todo')
@@ -99,11 +124,61 @@ libraryDatabaseInit()
 #   list all tracks
 # c. enter IDs for songs desired to send, in order you want them to play
 
-if choice == '1':
-    testSendFiles()
-else:
+# TODO display users
+def displayUsers():
+    for row in c.execute('SELECT * FROM users ORDER BY user_id'):
+        print(row[0], row[1])
+    uiPickUser()
+
+
+def displayLibrary():
+    print("\n\nDisplaying available tracks")
+    for row in c.execute('SELECT * FROM library ORDER BY track_id'):
+        print(row[0], row[1])
+
+
+def uiPickUser():
+    choice = input(
+        "Enter matching ID to user to select\nEnter 'add' to create a new user\nEnter 'exit' to exit :(\n")
+
+    if choice == 'exit':
+        print('error')  # TODO exit
+    if choice == 'add':
+        addUser()
+    else:
+        # get name from library, depending on if in history
+        # userID -> history -> library
+
+        #searchUser = int(choice)
+        # TODO remove
+        searchUser = 1
+
+        displayFileHistory(searchUser)
         # send to next ui step
-    reviewSentTracks()
+        uiPickTracks(searchUser)
+
+
+def uiPickTracks(userID):
+    displayLibrary()
+    choice = input(
+        "enter enter IDs for tracks, in the order you want them to play\nOr 'back' to go back to the ̶f̶u̶t̶u̶r̶e̶  user list\n")
+
+    if choice == 'exit':
+        print('error')  # TODO exit
+    if choice == 'back':
+        displayUsers()
+    else:
+        # parse choice into array of int
+        # trackList = choice.split(" ")
+        trackList = [14, 13, 21, 16]
+        # report size
+        print("Selected files are", getFileSizeTotal(trackList), "MB")
+        # send
+        drive = 'D'
+        sendFiles(trackList, userID, drive)
+
+
+displayUsers()
 ############# TEMP UI END #############
 
 conn.close()
