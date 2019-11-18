22b136b0d860142f1e9f6fb052d6b69a8fff97bf
IsaacS <greenace14@gmail.com>
2019-11-18 16:02:24 +0700

    +Change Table to List
    Allows much easier selection on user click

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,90 +1,202 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
+from PyQt5.Qt import *
+from PyQt5.QtGui import *
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from app import *
 
 import sys
 
+
+class TestListView(QListWidget):
+    dropped = pyqtSignal(list)
+
+    def __init__(self, type, parent=None):
+        super(TestListView, self).__init__(parent)
+        self.setAcceptDrops(True)
+        #self.setIconSize(QSize(72, 72))
+        #
+        #self.assetList = QListView(self)
+        self.clicked.connect(self.on_view_clicked)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+            links = []
+            for url in event.mimeData().urls():
+                links.append(str(url.toLocalFile()))
+            self.dropped.emit(links)
+        else:
+            event.ignore()
+
+    @pyqtSlot(QModelIndex)
+    def on_view_clicked(self, index):
+        # TODO selecting item logic
+        print((index.row()))
+
+
+class MainForm(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainForm, self).__init__(parent)
+
+        self.view = TestListView(self)
+        self.view.dropped.connect(self.pictureDropped)
+        self.setCentralWidget(self.view)
+
+    def pictureDropped(self, l):
+        for url in l:
+            if os.path.exists(url):
+                print(url)
+                icon = QIcon(url)
+                pixmap = icon.pixmap(72, 72)
+                icon = QIcon(pixmap)
+                item = QListWidgetItem(url, self.view)
+                item.setIcon(icon)
+                item.setStatusTip(url)
+
+
+# TODO get user's ID when clicking on table
+# TODO get USER's history, combine with library to show DONE tracks
+# TODO order tracks by folder (Tab?)
+# TODO implement drive selection
+# TODO implement file selection
+# TODO implement file total size-ing
+# TODO implement file ordering
+# TODO implement file sending
+# TODO implement disk unmount
 if __name__ == '__main__':
     libArr = getLibrary()
     userArr = getUsers()
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-   # window = QMainWindow()
-   # window.resize(250, 150)
-   # window.show()
+    window = QMainWindow()
+    window.resize(250, 150)
 
     #app = QApplication([])
-        
     ##### user layout #####
-    userWindow = QWidget()
+    userView = QWidget()
     userLayout = QVBoxLayout()
-    # establish height of table
-    userTable = QTableWidget(len(userArr), 4)
-    # headers
-    userTable.setHorizontalHeaderLabels(['ID', 'Person', 'Place','❌'])
-    # put users into table
-    for i, user in enumerate(userArr):
-        userTable.setItem(i, 0, QTableWidgetItem(str(user[0]))) #id
-        userTable.setItem(i, 1, QTableWidgetItem(user[1])) #name 
-        userTable.setItem(i, 2, QTableWidgetItem(user[2])) #loc
-        userTable.setItem(i, 3, QTableWidgetItem('❌'))
-    # add table to layout
-    userLayout.addWidget(userTable)
+
+    def userTable():
+        # establish height of table
+        table = QTableWidget(len(userArr), 4)
+        getPostsThread(table)
+        table.add_post.emit(self.scroll,
+                            SIGNAL("itemClicked(QTableWidgetItem *)"),
+                            self.updateMore
+                            )
+        # headers
+        table.setHorizontalHeaderLabels(['ID', 'Person', 'Place', '❌'])
+        # put users into table
+        table.setStyleSheet("QTableWidget{ background-color: lightgray;}")
+        for i, user in enumerate(userArr):
+            table.setItem(i, 0, QTableWidgetItem(str(user[0])))  # id
+            table.setItem(i, 1, QTableWidgetItem(user[1]))  # name
+            table.setItem(i, 2, QTableWidgetItem(user[2]))  # loc
+            table.setItem(i, 3, QTableWidgetItem('❌'))
+
+        return table
+
+    def userList():
+        # establish height of table
+        listView = QListWidget
+        listView = TestListView(listView)
+        for i, user in enumerate(userArr):
+            listView.insertItem(i, QListWidgetItem(
+                (user[1] + ", " + user[2]), listView))  # name + village
+        return listView
+    # add listView to layout
+    userLayout.addWidget(userList())
 
     lib_button = QPushButton('Library')
+
     def on_goLib_clicked():
-        libraryWindow.show()
+        mainStack.setCurrentIndex(1)
+        update_label(labelSelectedUser, "John shmoth")
     lib_button.clicked.connect(on_goLib_clicked)
     userLayout.addWidget(lib_button)
 
-
     userLayout.addWidget(QPushButton('Add User'))
-    userWindow.setLayout(userLayout)
-    userWindow.resize(500, 250)
-    userWindow.show()
+    userView.setLayout(userLayout)
     ##### END user Layout #####
 
     ##### Library Layout #####
-    libraryWindow = QWidget()
+    libraryView = QWidget()
     libraryLayout = QVBoxLayout()
 
-    # establish height of table
-    libTable = QTableWidget(len(libArr), 4)
-    # headers
-    libTable.setHorizontalHeaderLabels(['ID', 'Name', 'Size','❌'])
-    # put users into table
-    for i, track in enumerate(libArr):
-        libTable.setItem(i, 0, QTableWidgetItem(str(track[0]))) # id
-        libTable.setItem(i, 1, QTableWidgetItem(track[1])) # name 
-        libTable.setItem(i, 2, QTableWidgetItem((str(int(track[4]*1e-6))+ " MB"))) # size to MB
-        libTable.setItem(i, 3, QTableWidgetItem('❌'))
+    def update_label(self, user):
+        self.update = label_update(self, user)
+
+    def label_update(obj, user):  # obj is the object self
+        obj.setText(user)
+    labelSelectedUser = QLabel()
+    labelSelectedUser.setText("no User selected")
+    libraryLayout.addWidget(labelSelectedUser)
+
+    def libTable():
+        # establish height of table
+        table = QTableWidget(len(libArr), 4)
+        # headers
+        table.setHorizontalHeaderLabels(['ID', 'Name', 'Size', '❌'])
+        # put users into table
+        for i, track in enumerate(libArr):
+            table.setItem(i, 0, QTableWidgetItem(str(track[0])))  # id
+            table.setItem(i, 1, QTableWidgetItem(track[1]))  # name
+            table.setItem(i, 2, QTableWidgetItem(
+                (str(int(track[4]*1e-6)) + " MB")))  # size to MB
+            table.setItem(i, 3, QTableWidgetItem('❌'))
+        return table
     # add table to layout
-    libraryLayout.addWidget(libTable)
+    libraryLayout.addWidget(libTable())
 
     user_button = QPushButton('Back to Users')
+
     def on_goUserList_clicked():
-        userWindow.show()
+        mainStack.setCurrentIndex(0)
     user_button.clicked.connect(on_goUserList_clicked)
     libraryLayout.addWidget(user_button)
 
     libraryLayout.addWidget(QPushButton('Refresh Tracks'))
-    libraryWindow.setLayout(libraryLayout)
-    libraryWindow.resize(500, 250)
-    libraryWindow.show()
+    libraryView.setLayout(libraryLayout)
     ##### END Library #####
 
+    #### stack the views ####
+    mainStack = QStackedWidget()
+    mainStack.addWidget(userView)
+    mainStack.addWidget(libraryView)
+    ###### views END #######
+
+    mainLayout = QVBoxLayout()
+    mainLayout.addWidget(mainStack)
+    switchWindow = QWidget()
+    switchWindow.resize(500, 300)
+    switchWindow.setLayout(mainLayout)
+    switchWindow.show()
     ####################################
-    app = QApplication([])
-    button = QPushButton('Click')
-    def on_button_clicked():
-        alert = QMessageBox()
-        alert.setText('You clicked the button!')
-        alert.exec_()
+    # app = QApplication([])
+    # button = QPushButton('Click')
+    # def on_button_clicked():
+    #    alert = QMessageBox()
+    #    alert.setText('You clicked the button!')
+    #    alert.exec_()
 
-    button.clicked.connect(on_button_clicked)
-    button.show()
+    # button.clicked.connect(on_button_clicked)
+    # button.show()
     ##################################
-
 
     # app.exec_() #can't use while testing in powershell
 
